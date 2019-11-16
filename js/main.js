a3cf96d8e1b5122e49fc16d2827db9ed2a965843
HaisM <32810914+mrcracken@users.noreply.github.com>
2019-11-16 16:52:37 +0300

    Commit 1

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*Created for GitHub tutorial 2019*/
+
+function sayHelloOrWorld(){
+	var userInput = document.getElementById('userInput').value;
+	if (userInput == "Hello"){
+		alert("World");
+	}
+	else {
+		if (userInput == "World!"){
+			alert("Hello!");
+		}
+		else{
+			alert("Error!");
+		}
+	}
+}
